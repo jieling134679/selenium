a78f553a8b37e92d6d2d8ac27901033ffadab0b1
jieling134679 <274712917@qq.com>
2021-01-16 17:03:10 +0800

    pycharm

--- webauto.py
+++ webauto.py
@@ -1,2 +1,11 @@
 from selenium import webdriver
 wd = webdriver.Chrome(r'D:\Python\selenim\chromedriver.exe')
+wd.get('http://www.baidu.com')
+
+print('你好')
+
+element = wd.find_element_by_id('kw')
+element.send_keys('中文')
+
+element = wd.find_element_by_id('su')
+element.click()
