5bbd3e466816ed96a64e04fce79ba929cef107fa
jieling134679 <274712917@qq.com>
2021-01-17 19:12:43 +0800

    py

--- webauto.py
+++ webauto.py
@@ -2,10 +2,12 @@ from selenium import webdriver
 wd = webdriver.Chrome(r'D:\Python\selenim\chromedriver.exe')
 wd.get('http://www.baidu.com')
 
-print('你好')
 
 element = wd.find_element_by_id('kw')
-element.send_keys('中文')
+element.send_keys('成都师范学院')
 
 element = wd.find_element_by_id('su')
 element.click()
+
+
+
