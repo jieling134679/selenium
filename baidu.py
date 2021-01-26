d9e18589253466c356f3991a4ce5befdf6efde4d
jieling134679 <274712917@qq.com>
2021-01-26 23:37:38 +0800

    3

--- baidu.py
+++ baidu.py
@@ -3,10 +3,9 @@ wd = webdriver.Chrome(r'D:\Python\selenim\chromedriver.exe')
 wd.implicitly_wait(10)
 
 wd.get('https://www.baidu.com/')
-wd.find_element_by_id('kw').send_keys('成都房价\n')
+wd.find_element_by_id('kw').send_keys('双流房价\n')
 
 # import time
 # time.sleep(1)
 
-element = wd.find_element_by_id('1')
-print(element.text)
+
